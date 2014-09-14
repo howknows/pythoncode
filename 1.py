5585e20908022aa1512496b193acd02c179e66da
whoknows <zhenhaohong@gmail.com>
2014-09-15 01:21:39 +0800

    python

--- 1.py
+++ 1.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
-print "io".upper()
+class fooclass(object):
+	"""docstring for fooclass"""
+	version = 0.1
+	def __init__(self,nm="ok!"):
+		self.name = nm
+		print "1 ",nm
+	def  show(self):
+		print self.name
+		print  self.__class__.__name__
+foo = fooclass()
+foo.show()
